d4f5adba337886d8ec1438d83815f9ad379421dc
jjb8966 <jjb8966@naver.com>
2022-07-04 23:45:54 +0900

    feat: 1장 프로젝트 환경설정

---
+++ jpashop-project/src/main/java/jpabook/jpashopproject/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashopproject;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;   // 스프링 컨테이너가 entity manager 주입해줌
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ jpashop-project/src/test/java/jpabook/jpashopproject/JpashopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
