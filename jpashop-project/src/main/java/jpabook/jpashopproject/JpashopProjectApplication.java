e22ff4bfd846be08af30478b62a04d43e12c3a03
jjb8966 <jjb8966@naver.com>
2022-07-04 23:44:11 +0900

    initial commit

---
+++ jpashop-project/src/main/java/jpabook/jpashopproject/JpashopProjectApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopProjectApplication.class, args);
+	}
+
+}
